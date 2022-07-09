43a70c7b37a609c38325ed9a2dc533a7d1f50b98
Jiaxiao Zhou <jiazho@microsoft.com>
2022-07-09 11:33:34 -0700

    added wasm dependency to devcontainer
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -18,6 +18,10 @@ rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
+rustup update stable --no-self-update
+rustup default stable
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-wasi
 
 cargo install cargo-expand
 cargo install cargo-edit
@@ -28,3 +32,14 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+# Install Python stuff
+printf "Installing Python Dependencies"
+pip install mypy wasmtime
+
+# Install NPM dependencies
+printf "Installing NPM Dependencies"
+cd crates/gen-js && npm install
+
+# Install wit-bindgen CLI
+cargo install --git https://github.com/bytecodealliance/wit-bindgen wit-bindgen-cli --rev a79a4be33d76ddf62839ba71602c26a96610ef7c
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use wasmtime::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let engine = Engine::default();
+    let module = Module::from_file(&engine, "hello.wat")?;
+    let mut store = Store::new(&engine, ());
+    let instance = Instance::new(&mut store, &module, &[])?;
+    let answer = instance
+        .get_func(&mut store, "answer")
+        .expect("`answer` was not an exported function");
+    let answer = answer.typed::<(), i32, _>(&store)?;
+    let result = answer.call(&mut store, ())?;
+    println!("Answer: {:?}", result);
+    Ok(())
 }
